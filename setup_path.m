80f75198bb7718197f7afecafe4eab93410bf377
sunnymilk- <orb33@yahoo.com>
2015-01-04 09:40:00 -0500

    more paths

--- setup_path.m
+++ setup_path.m
@@ -1,12 +1,13 @@
 function setup_path
+    root_path = pwd;
 
-    addpath('./');
-    addpath('./scripts/');
-    addpath('./depth/');
-    addpath('./plane_fitting/');
-    addpath('./cell_data_funcs/');
+    addpath(root_path);
+    addpath([root_path '/scripts/']);
+    addpath([root_path '/depth/']);
+    addpath([root_path '/plane_fitting/']);
+    addpath([root_path '/cell_data_funcs/']);
 
-    addpath('./tree_analysis/');
+    addpath([root_path './tree_analysis/']);
 %     
 %     addpath('~greenem/stratification/matlab_bgl/');
 %     addpath('~greenem/stratification/DSDP5.8_64/matlab/');
