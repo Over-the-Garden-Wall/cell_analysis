baa62796c268aeb4b8abf1b00557c6be7271e5f6
matt <orb33@yahoo.com>
2016-02-21 17:31:26 -0500

    mean point

--- cell_data.m
+++ cell_data.m
@@ -676,3 +676,13 @@ function soma_point = get_soma_loc(cell_id)
         
     
 end
+
+function mean_point = get_mean_point(cell_num, use_soma)
+
+    if use_soma
+        mean_point = get_soma_loc(cell_num); 
+    else
+        mean_point = get_mean_loc(cell_num);
+    end
+    
+end
--- get_mean_point.m
+++
@@ -1,9 +0,0 @@
-function mean_point = get_mean_point(cell_num, use_soma)
-
-    if use_soma
-        mean_point = get_soma_loc(cell_num); 
-    else
-        mean_point = get_mean_loc(cell_num);
-    end
-    
-end
